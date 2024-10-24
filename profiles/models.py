2c3c43677cd36229c65e565c817e26f300ec59ad
Thomas <mergener.thomas@gmail.com>
2024-10-24 03:03:26 -0300

    Remove display name from follower model

--- profiles/models.py
+++ profiles/models.py
@@ -4,12 +4,9 @@ from django.contrib import admin
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    display_name = models.CharField(max_length=100)
     bio = models.TextField(blank=True) 
     join_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.display_name
 
 class Follower(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
